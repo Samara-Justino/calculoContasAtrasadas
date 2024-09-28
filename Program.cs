99ce9696c6ca2485ed56b0331ceeb8d40b1c7ca9
Samara Justino <samarasouzajustino@gmail.com>
2024-09-28 17:37:51 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Qual o salário do João R$? ");
+double salario = double.Parse(Console.ReadLine());
+Console.Write("Qual o valor da primeira conta R$ ");
+double conta1 = double.Parse(Console.ReadLine());
+Console.Write("Qual o valor da segunda conta R$ ");
+double conta2 = double.Parse(Console.ReadLine());
+
+double salarioRestante = salario - ( (conta1 + (conta1 * (2.0 / 100.0))) + (conta2 + (conta2 * (2.0/100.0))) );
+Console.WriteLine("O salário restante é R$ " + salarioRestante.ToString("F2"));
